7783d66eaa9bb0bf8b64f368dce595288160672e
Jeff Wyckoff <jokerswild663@gmail.com>
2015-01-11 23:41:46 -0800

    simple java program for compilation

---
+++ extra.java
@@ -0,0 +1,9 @@
+class Extras {
+	int added() {
+		int first = 2;
+		int second = 3;
+		int third = first + second;
+
+		return third;
+	}
+}
---
+++ testing.java
@@ -0,0 +1,10 @@
+
+class Hello {
+	public static void main(String[] args) {
+		Extras stuff = new Extras();
+		int number = stuff.added();
+		System.out.println("hello world");
+		System.out.println("second message\n");
+		System.out.println(number);
+	}
+}
